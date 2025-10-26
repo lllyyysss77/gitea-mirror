5876198b5e401aef82963296bb4c0a6760f3e04a
Arunavo Ray <rayanup3@gmail.com>
2025-10-26 18:36:20 +0530

    Added missing DB fields

---
+++ drizzle/0006_military_la_nuit.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `accounts` ADD `id_token` text;--> statement-breakpoint
+ALTER TABLE `accounts` ADD `access_token_expires_at` integer;--> statement-breakpoint
+ALTER TABLE `accounts` ADD `refresh_token_expires_at` integer;--> statement-breakpoint
+ALTER TABLE `accounts` ADD `scope` text;
